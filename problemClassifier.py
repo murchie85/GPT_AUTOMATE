9b92bb9d7b6e0f01d94f4feac6a3c810df831513
murchie85 <murchie85@gmail.com>
2023-04-12 08:37:45 +0100

    adding openai key from var

--- problemClassifier.py
+++ problemClassifier.py
@@ -135,8 +135,9 @@ class ProblemClassifier:
         # ----------------- CALL OPEN AI
 
         #keyFile = open('/Users/adammcmurchie/code/tools/davinci/.KEY.txt','r')
-        keyFile = open('.TOKEN.txt','r')
-        openai.api_key    = keyFile.read()
+        #keyFile = open('.TOKEN.txt','r')
+        #openai.api_key      = keyFile.read()
+        openai.api_key       = os.environ["OPENAI_API_KEY"]
 
 
         availableResponses   = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "system", "content": prompt}])
