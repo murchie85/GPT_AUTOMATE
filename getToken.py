204eff36e2cc7759eee919b639322d7cbfe23c8f
murchie85 <murchie85@gmail.com>
2023-04-15 20:36:00 +0100

    changing add token

--- getToken.py
+++ getToken.py
@@ -11,8 +11,8 @@ def getOpenAPIKey():
         openai.api_key = os.environ["OPENAI_API_KEY"]
     else:
         cprint('No key in env vars, attempting to load from file ','yellow')
-        keyFile = open('/Users/adammcmurchie/code/tools/davinci/.KEY.txt','r')
-        #keyFile = open('.TOKEN.txt','r')
+        #keyFile = open('/Users/adammcmurchie/code/tools/davinci/.KEY.txt','r')
+        keyFile = open('.TOKEN.txt','r')
         openai.api_key      = keyFile.read()
     
     print('')
